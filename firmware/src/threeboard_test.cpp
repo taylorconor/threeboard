37add7ea93c4bc1cd9b06ac9aae601eb6f05763a
Conor Taylor <conor@taylor.sh>
2021-04-06 12:44:29 +0200

    Add constructor expectations to ThreeboardTest

--- firmware/src/threeboard_test.cpp
+++ firmware/src/threeboard_test.cpp
@@ -8,12 +8,20 @@
 #include "src/native/native_mock.h"
 #include "src/usb/usb_mock.h"
 
+using ::testing::_;
+using ::testing::Return;
+
 namespace threeboard {
 namespace {
 
 class ThreeboardTest : public ::testing::Test {
  public:
   ThreeboardTest() {
+    EXPECT_CALL(native_mock_, SetTimerInterruptHandlerDelegate(_)).Times(1);
+    EXPECT_CALL(native_mock_, EnableTimer1()).Times(1);
+    EXPECT_CALL(native_mock_, EnableTimer3()).Times(1);
+    EXPECT_CALL(led_controller_mock_, GetLedState())
+        .WillOnce(Return(&led_state_));
     threeboard_ = std::make_unique<Threeboard>(
         &native_mock_, &usb_mock_, &event_buffer_, &led_controller_mock_,
         &key_controller_mock_);
@@ -24,6 +32,7 @@ class ThreeboardTest : public ::testing::Test {
   EventBuffer event_buffer_;
   LedControllerMock led_controller_mock_;
   KeyControllerMock key_controller_mock_;
+  LedState led_state_;
   std::unique_ptr<Threeboard> threeboard_;
 };
 
