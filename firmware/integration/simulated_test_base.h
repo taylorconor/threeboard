41ee07d6df012911ba3fdea98a2a21468c8a3dd5
Conor Taylor <conor@taylor.sh>
2021-05-22 18:10:02 +0200

    Temporary fix for macOS linker issue in sim_test

--- firmware/integration/simulated_test_base.h
+++ firmware/integration/simulated_test_base.h
@@ -3,6 +3,7 @@
 #include <chrono>
 
 #include "gtest/gtest.h"
+#include "simavr/sim_elf.h"
 #include "simulator/simavr/simavr_impl.h"
 
 namespace threeboard {
@@ -10,7 +11,14 @@ namespace integration {
 
 class SimulatedTestBase : public ::testing::Test {
  public:
-  SimulatedTestBase() { simavr_ = simulator::SimavrImpl::Create(); }
+  SimulatedTestBase() {
+    // TODO: without this call to elf_read_firmware, the macOS dynamic linker
+    // fails with "dyld: Symbol not found: _elf_read_firmware". This symbol is
+    // located within libsimavr.a, but there must be a better way to force this
+    // to be linked.
+    elf_read_firmware(nullptr, nullptr);
+    simavr_ = simulator::SimavrImpl::Create();
+  }
 
   void Run(const std::chrono::milliseconds& timeout =
                std::chrono::milliseconds(10)) {
