68e1e99abc2bf8afa6b2bd28f77902c51a9bd454
Conor Taylor <conor@taylor.sh>
2021-05-23 14:35:53 +0200

    Add initial test case

--- firmware/integration/simulated_test_base.h
+++ firmware/integration/simulated_test_base.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <chrono>
+#include <unordered_map>
 
 #include "gtest/gtest.h"
 #include "simavr/sim_elf.h"
@@ -11,15 +12,20 @@ namespace integration {
 
 class SimulatedTestBase : public ::testing::Test {
  public:
-  SimulatedTestBase() {
+  SimulatedTestBase() { simavr_ = simulator::SimavrImpl::Create(); }
+
+  static void SetUpTestCase() {
+    // Parse the symbols from the elf file and build a symbol table out of them.
+    BuildSymbolTable();
+
     // TODO: without this call to elf_read_firmware, the macOS dynamic linker
     // fails with "dyld: Symbol not found: _elf_read_firmware". This symbol is
     // located within libsimavr.a, but there must be a better way to force this
     // to be linked.
     elf_read_firmware(nullptr, nullptr);
-    simavr_ = simulator::SimavrImpl::Create();
   }
 
+ protected:
   void Run(const std::chrono::milliseconds& timeout =
                std::chrono::milliseconds(10)) {
     auto start = std::chrono::system_clock::now();
@@ -41,8 +47,25 @@ class SimulatedTestBase : public ::testing::Test {
     return false;
   }
 
- protected:
+  bool RunUntil(const std::string& symbol,
+                const std::chrono::milliseconds& timeout =
+                    std::chrono::milliseconds(10)) {
+    auto start = std::chrono::system_clock::now();
+    while (timeout > std::chrono::system_clock::now() - start) {
+      if (symbol == "todo") {
+        return true;
+      }
+      simavr_->Run();
+    }
+    return false;
+  }
+
   std::unique_ptr<simulator::Simavr> simavr_;
+
+ private:
+  static void BuildSymbolTable() {}
+
+  static std::unordered_map<std::string, uint32_t> symbol_table_;
 };
 
 }  // namespace integration
