555477d75ad639bb5bf3e3363003fcabbcfa61e0
Conor Taylor <conor@taylor.sh>
2021-08-23 22:13:50 +0200

    Refactor Keypress into a standalone header

---
+++ firmware/src/keypress.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stdint.h>
+
+namespace threeboard {
+
+enum class Keypress : uint8_t {
+  // Define an inactive keypress (no keys pressed) to differentiate between
+  // active and inactive states in handler delegates. The KeypressHandler will
+  // never be called with an INACTIVE keypress.
+  INACTIVE = 0,
+  // Individual keypresses.
+  Z = 1,
+  Y = 2,
+  X = 4,
+  // Two-key combos.
+  YZ = 3,
+  XZ = 5,
+  XY = 6,
+  // Three-key combo.
+  XYZ = 7,
+};
+}
