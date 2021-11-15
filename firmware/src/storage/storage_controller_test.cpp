018c59a048f9bb0f9f7ce99bfbf6dec93dca08e5
Conor Taylor <conor@taylor.sh>
2021-11-15 22:04:40 +0100

    Add unimplemented StorageController test file

---
+++ firmware/src/storage/storage_controller_test.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+
+namespace threeboard {
+namespace storage {
+namespace {
+
+class StorageControllerTest : public ::testing::Test {};
+
+TEST_F(StorageControllerTest, DoNothing) {}
+
+}  // namespace
+}  // namespace storage
+}  // namespace threeboard
